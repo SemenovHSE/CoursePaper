fcf28467f76baa94e50696146f8fa2f73e24a239
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2020-03-24 11:51:44 +0500

    updated server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mssqlConfig = require("./config/MSSQLConfig")
 const MSSQLWorker = require("./application/classes/MSSQLWorker");
 
 const app = express();
-const port = 8000;
+const port = 443;
 
 (async function() {
     try {
